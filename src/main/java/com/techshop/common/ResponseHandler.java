ae64deb8bc634dc4aa3f1f3238d83bf1d41b8cb0
Phạm Tấn khang <phamtankhang2001@gmail.com>
2022-02-12 13:43:59 +0700

    setup source code

---
+++ src/main/java/com/techshop/common/ResponseHandler.java
@@ -0,0 +1,43 @@
+package com.techshop.common;
+
+import com.techshop.common.util.DateUtils;
+import com.techshop.common.util.ErrorUtils;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
+
+import java.time.LocalDateTime;
+import java.util.HashMap;
+import java.util.Map;
+
+public class ResponseHandler {
+    public static ResponseEntity<Object> getResponse(Object content, HttpStatus status){
+        Map<String, Object> map = new HashMap<>();
+        map.put("content", content);
+        map.put("errors", "");
+        map.put("timestamp", DateUtils.toString(LocalDateTime.now()));
+        map.put("status", status.value());
+
+        return new ResponseEntity<Object>(map, status);
+    }
+
+    public static ResponseEntity<Object> getResponse(BindingResult errors, HttpStatus status){
+        Map<String, Object> map = new HashMap<>();
+        map.put("content", "");
+        map.put("errors", ErrorUtils.getErrorMessages(errors));
+        map.put("timestamp", DateUtils.toString(LocalDateTime.now()));
+        map.put("status", status.value());
+
+        return new ResponseEntity<Object>(map, status);
+    }
+
+    public static Object getResponse(HttpStatus status) {
+        Map<String, Object> map = new HashMap<>();
+        map.put("content", "");
+        map.put("errors", "");
+        map.put("timestamp", DateUtils.toString(LocalDateTime.now()));
+        map.put("status", status.value());
+
+        return new ResponseEntity<Object>(map, status);
+    }
+}
--- src/main/java/com/techshop/config/JpaConfig.java
+++ src/main/java/com/techshop/config/JpaConfig.java
@@ -1,10 +1,7 @@
-package com.techshop.common.util;
+package com.techshop.config;
 
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.data.domain.AuditorAware;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
-import org.springframework.security.core.context.SecurityContextHolder;
 
 import java.util.Optional;
 
